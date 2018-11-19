e416e60755b7c61f2947bcde966f74bab1b7a224
mhd-rawashdah <mhd.rawashdah@gmail.com>
2018-11-20 00:11:15 +0200

    finsish the backend side

--- database/index.js
+++ database/index.js
@@ -2,15 +2,39 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
-});
+	  	username:String,
+	  	repos:Object
+	});
 
-let Repo = mongoose.model('Repo', repoSchema);
+	let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
-}
+	  	let save = (data) => {
+	  		// TODO: Your code here
+	  		// This function should save a repo or repos to
+	  		// the MongoDB
+	  		console.log(data.username)
+
+	  		console.log("db")
+
+	  		// console.log(data)
+	  		let repos = new Repo(data);
+	  		
+	 	 	repos.save(function(err){
+	  			if (err) {
+	     			console.log("error")
+	  	  			return handleError(err)
+	    		}
+	  			console.log('Saved!')
+	 		})
+    	}
+
+    	let get=(username,cb)=>{
+           Repo.findOne({username:username} , function(err,data){
+           	cb(data);
+           });
+
+    	}
+
 
 module.exports.save = save;
+module.exports.get=get
--- server/index.js
+++ server/index.js
@@ -1,18 +1,50 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongo = require('../database/index.js');
+const helpers = require('../helpers/github.js');
 let app = express();
+
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.post('/repos', function (req, res) {
-  // TODO - your code here!
-  // This route should take the github username provided
-  // and get the repo information from the github API, then
-  // save the repo information in the database
+	console.log("server post")
+   // TODO - your code here!
+   // This route should take the github username provided
+   // and get the repo information from the github API, then
+   // save the repo information in the database
+  var username = req.body.username;
+
+  mongo.get(username, function(data){
+  	if (data){
+  		res.sendStatus(201)
+  	}
+  	else{
+	  	helpers.getReposByUsername(username, function(userRepos){
+	  		console.log("server to db")
+			if (userRepos.repos.length > 0) {
+				mongo.save(userRepos);
+	        	res.send(userRepos); 
+			} else {
+			res.send('This username has no repo')
+			}
+	  	});
+	  
+		}
+  })
+
 });
 
+
+ 
 app.get('/repos', function (req, res) {
-  // TODO - your code here!
-  // This route should send back the top 25 repos
+    // TODO - your code here!
+    // This route should send back the top 25 repos
+	mongo.get(req.body.username,function(data){
+		console.log("mongo data " + data);
+		res.send(req.body.username)
+	})
 });
 
 let port = 1128;
